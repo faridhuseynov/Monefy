bd88e0051c37eb87e3d1ff14cf03c006aa183743
faridhuseynov <35901226+faridhuseynov@users.noreply.github.com>
2018-07-12 22:07:47 +0400

    Updated Class Account by adding list of categories and currency member

--- Monefy/Account.cs
+++ Monefy/Account.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Monefy
 {
+    enum CURR { AZN, USD, EURO };
     class Account
     {
         //account name
@@ -14,10 +15,17 @@ namespace Monefy
         //account's money amount
         public int Money { get; set; }
 
+        //account's money currency
+        public CURR Currency { get; set; }
+
         //account's hidden balance
         public int HiddenBalance { get; set; }
 
         //accounts ID
         public int ID { get; set; }
+
+        //list of categories
+        List<Category> categories;
+
     }
 }
