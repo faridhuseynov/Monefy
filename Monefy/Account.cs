8ba3dc661a142e40591b4f5ea353e8543c70deba
faridhuseynov <35901226+faridhuseynov@users.noreply.github.com>
2018-07-12 21:17:31 +0400

    Class Account created

---
+++ Monefy/Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monefy
+{
+    class Account
+    {
+        //account name
+        public string Name { get; set; }
+
+        //account's money amount
+        public int Money { get; set; }
+
+        //account's hidden balance
+        public int HiddenBalance { get; set; }
+
+        //accounts ID
+        public int ID { get; set; }
+    }
+}
--- Monefy/Program.cs
+++ Monefy/Program.cs
@@ -10,7 +10,7 @@ namespace Monefy
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Hello");
         }
     }
 }
