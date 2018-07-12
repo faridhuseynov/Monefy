5eb70149e8116b6109d37d5967b393a966fc1729
faridhuseynov <35901226+faridhuseynov@users.noreply.github.com>
2018-07-12 21:32:04 +0400

    Class Category created

---
+++ Monefy/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monefy
+{
+    enum Type { Expense, Income}
+    class Category
+    {
+        //category name
+        public string Name { get; set; }
+
+        //category Type
+        public Type type { get; set; }
+
+        //category ID
+        public int ID { get; set; }
+
+        //money spent on category
+        public int MoneySpent { get; set; }
+    }
+}
